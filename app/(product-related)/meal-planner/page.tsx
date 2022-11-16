a07abaefd5e2de1a106c45526e15e311dc9d0afe
badkamyk <slothmateusz@gmail.com>
2022-11-16 15:01:03 +0100

    meal planner icons

--- app/(product-related)/meal-planner/page.tsx
+++ app/(product-related)/meal-planner/page.tsx
@@ -67,11 +67,11 @@ export default function MealPlanner() {
                             {renderNutrients(value.nutrients)}
                         </div>
                         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                            {value.meals.map((meal: DayMealType, index) => {
+                            {value.meals.map((meal: DayMealType) => {
                                 return (
                                     <div
                                         key={meal.id}
-                                        className="bg-white rounded-lg shadow-lg hover:bg-blue-200 hover:cursor-pointer p-6"
+                                        className="bg-white rounded-lg shadow-lg hover:bg-blue-200 hover:cursor-pointer p-9 relative"
                                     >
                                         <Link href={`/recipe/${meal.id}`}>
                                             <h3 className="text-xl font-bold">{meal.title}</h3>
@@ -91,9 +91,12 @@ export default function MealPlanner() {
                                             {/*        </p>*/}
                                             {/*    );*/}
                                             {/*})}*/}
-
-
                                         </Link>
+                                        <div className="absolute w-6 h-6 top-3 right-5">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" fill="none" className="w-6 h-6 hover:fill-red-500">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                                            </svg>
+                                        </div>
                                     </div>
                                 );
                             })}
