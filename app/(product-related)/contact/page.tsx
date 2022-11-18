7a4e0d76758e3fe71e6e65ec5e61d90750a78825
badkamyk <slothmateusz@gmail.com>
2022-11-18 11:12:45 +0100

    added controlled inputs for contact page

--- app/(product-related)/contact/page.tsx
+++ app/(product-related)/contact/page.tsx
@@ -1,25 +1,42 @@
+'use client'
+import {useState, useRef} from "react";
+
 export default function Contact() {
+    const [formInputs, setFormInputs] = useState({
+        email: "",
+        subject: "",
+        message: "",
+    });
+    const formRef = useRef<HTMLFormElement>(null);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const {name, value} = e.target;
+        setFormInputs((prev: typeof formInputs) => ({...prev, [name]: value}));
+    };
+
+
     return (
         <section className="bg-white dark:bg-gray-900">
             <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
                 <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact
                     Us</h2>
                 <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a
-                    technical issue? Want to send feedback about a beta feature? Need details about our Business plan?
+                    technical issue? Want to send feedback about a feature? Need details about our Business plan?
                     Let us know.</p>
-                <form action="#" className="space-y-8">
+                <form ref={formRef} action="#" className="space-y-8">
                     <div>
                         <label htmlFor="email"
                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your
                             email</label>
-                        <input type="email" id="email"
+                        <input type="email" id="email" onChange={handleChange} name="email" value={formInputs.email}
                                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                               placeholder="name@flowbite.com" required/>
+                               placeholder="name@example.com" required/>
                     </div>
                     <div>
                         <label htmlFor="subject"
                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
-                        <input type="text" id="subject"
+                        <input type="text" id="subject" onChange={handleChange} name="subject"
+                               value={formInputs.subject}
                                className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                                placeholder="Let us know how we can help you" required/>
                     </div>
@@ -27,9 +44,9 @@ export default function Contact() {
                         <label htmlFor="message"
                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your
                             message</label>
-                        <textarea id="message" rows={6}
+                        <textarea id="message" rows={6} onChange={handleChange} name="message" value={formInputs.message}
                                   className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                  placeholder="Leave a comment..."></textarea>
+                                  placeholder="Leave a comment..."/>
                     </div>
                     <button type="submit"
                             className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-blue-700 sm:w-fit hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send
