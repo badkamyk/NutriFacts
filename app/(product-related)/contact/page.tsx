ad1eda4c09893f0e9853796e8349f726520cfe88
badkamyk <slothmateusz@gmail.com>
2022-12-29 12:33:50 +0100

    style: input sizes

--- app/(product-related)/contact/page.tsx
+++ app/(product-related)/contact/page.tsx
@@ -49,7 +49,8 @@ export default function Contact() {
                         {/*       className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"*/}
                         {/*       placeholder="name@example.com" required/>*/}
                         <Input type="email" id="email" onChange={handleChange} name="email" value={formInputs.email}
-                               placeholder="name@example.com" required/>
+                               placeholder="name@example.com" required
+                               className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"/>
                     </div>
                     <div>
                         <label htmlFor="subject"
--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -118,11 +118,11 @@ export default function FindRecipe({ setIngredients, setRecipe, chosenCategory, 
                         {/*       className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"*/}
                         {/*       placeholder="Ingredients..."/>*/}
                         <Input
-                            className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+                            className="block p-4 w-full z-20 text-md text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
                             onChange={handleSearch} value={search} type="search" id="search-dropdown" ref={inputRef}
                             placeholder={"Ingredients..."}/>
                         <button type="submit"
-                                className="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                className="absolute top-0 right-0 bottom-0 p-3.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             <svg aria-hidden="true" className="w-5 h-5" fill="none" stroke="currentColor"
                                  viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
