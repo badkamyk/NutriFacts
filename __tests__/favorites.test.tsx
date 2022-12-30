3a21a4cedfaab709578892e1babb93c8a3d59433
badkamyk <slothmateusz@gmail.com>
2022-12-30 14:00:02 +0100

    fix/test: fixed favorites component fetching issue

--- __tests__/favorites.test.tsx
+++ __tests__/favorites.test.tsx
@@ -3,8 +3,7 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { navTest } from "./nav.test";
 import { footerTest } from "./footer.test";
-import fetch from "node-fetch";
-
+import fetch from "jest-fetch-mock";
 
 describe("FavoritePage", () => {
     beforeEach(() => {
